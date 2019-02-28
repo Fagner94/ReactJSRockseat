a0bd0163015d07bace3f5924daa54b9b2a9cd689
Fagner94 <fagnerfreire94@hotmail.com>
2019-02-28 17:19:01 -0300

    Add files via upload

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./styles.css";
+
+// semelhante a 
+/**
+ * class Header extends Component{
+ * render(){
+ *  return <h1>exemplo</h1>
+ * }
+ * }
+ * 
+ * 
+ * 
+ */
+
+const Header = () =>(
+
+<header id="main-header">Cabeçalho</header>
+
+);
+
+export default Header;
